0d4ab3b3f3f18bb90e916d3cc54b9325a84cdbda
Wouter <w.h.v.d.sluis@student.tue.nl>
2020-05-23 11:50:47 +0200

    PathPlannerModel package was added

---
+++ 4AT100_V2/ImportSyncSimulink.m
@@ -0,0 +1,3 @@
+addpath('C:\ProgramData\IBM\Rational\Rhapsody\8.4\Share\etc');
+addpath('C:\Users\woute\stack\Documenten\Master AT\4AT100\model files');
+SimulinkDataExtractor('C:\Users\woute\stack\Documenten\Master AT\4AT100\model files\PathPlanningSample.slx', 'C:\Users\woute\stack\Documenten\Master AT\4AT100\Group4-ibm\4AT100_V2\SimDataFile.txt');
